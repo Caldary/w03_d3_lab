403ec4339216028079902fee7d4cd245e037e7f5
Valerian Roustit <valerian.roustit@gmail.com>
2020-04-15 16:29:54 +0100

    add save method

--- db/console.rb
+++ db/console.rb
@@ -2,8 +2,11 @@ require ( 'pry' )
 require_relative('../models/album')
 require_relative('../models/artist')
 
+artist1 = Artist.new( { "name" => "Gorillaz" } )
+artist1.save()
 
-
+album1 = Album.new( {'title' => 'Clint Eastwood', 'genre' => 'pop', 'artist_id' => artist1.id} )
+album1.save()
 
 
 binding.pry
