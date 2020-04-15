bd502130ec29f866e625066f989bb715f247ff3b
Valerian Roustit <valerian.roustit@gmail.com>
2020-04-15 16:15:59 +0100

    initialize classes

---
+++ models/artist.rb
@@ -0,0 +1,15 @@
+require('pg')
+require_relative('../db/sql_runner')
+require_relative('album')
+
+class Artist
+    attr_accessor :name
+    attr_reader :id
+
+    def initialize(options)
+        @id = options['id'] if options['id']
+        @name = options['name']
+    end
+        
+
+end
