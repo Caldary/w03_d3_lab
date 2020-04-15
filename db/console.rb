4d2b414f6c739fbc2ef2f3ee80a9ebf07992df93
Valerian Roustit <valerian.roustit@gmail.com>
2020-04-15 16:46:27 +0100

    add list all method

--- db/console.rb
+++ db/console.rb
@@ -5,8 +5,14 @@ require_relative('../models/artist')
 artist1 = Artist.new( { "name" => "Gorillaz" } )
 artist1.save()
 
+artist2 = Artist.new( {"name" => "Metalica"} )
+artist2.save()
+
+
 album1 = Album.new( {'title' => 'Clint Eastwood', 'genre' => 'pop', 'artist_id' => artist1.id} )
+album2 = Album.new( {'title' => 'Melancholy hill', 'genre' => 'pop', 'artist_id' => artist1.id} )
 album1.save()
+album2.save()
 
 
 binding.pry
